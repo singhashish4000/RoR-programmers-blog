b931421345a8f12c312bdb46d95d0fd0b7b8681d
singhashish4000 <singhashish4000@gmail.com>
2018-01-26 22:20:54 +0530

    create show action and add gravator

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+    def gravator_for(user ,size)
+       gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+       size = 100
+       gravator_url = url = "http://gravatar.com/avatar/#{gravator_id}?s=#{size}"
+       image_tag(gravator_url, alt: user.username, class: "img-circle") 
+    end    
 end
