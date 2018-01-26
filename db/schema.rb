23e64d3b3ab851abdacee6415aff1910999bb5c6
singhashish4000 <singhashish4000@gmail.com>
2018-01-26 19:14:22 +0530

    add association between user and artilce and add debugger

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126083919) do
+ActiveRecord::Schema.define(version: 20180126131633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180126083919) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "tests", force: :cascade do |t|
